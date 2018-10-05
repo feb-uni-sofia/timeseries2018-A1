e2bdbc8d1ece1f3e128c1b46faf19d6b744cf528
Boyko Amarov <boyko.amarov@gmail.com>
2018-10-05 10:54:46 +0300

    Update Problem_2.R

---
+++ Problem_2.R
@@ -0,0 +1,33 @@
+# a)
+# install.packages('xts')
+library(xts)
+
+# b) 
+
+bitcoins <- read.csv(file = 'https://s3.eu-central-1.amazonaws.com/sf-timeseries/data/bitcoin2017-06-01-2017-11-13.csv',
+                     stringsAsFactors = FALSE)
+str(bitcoins)
+
+# c) 
+
+# d) 
+
+# e)
+
+# f)
+
+# g) 
+
+# h)
+
+# i)
+
+# j)
+
+# k)
+
+# l) Ljung–Box test for autocorrelation of up to order 10
+
+# m)
+
+# n)
