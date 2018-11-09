be13bfa344269b9a1ae9e168eef0e7f4fd4e65d9
Boyko Amarov <boyko.amarov@gmail.com>
2018-11-09 12:24:45 +0200

    Update Problem_2.R

--- Problem_2.R
+++ Problem_2.R
@@ -10,14 +10,35 @@ str(bitcoins)
 
 # c) 
 
-# d) 
+btc <- xts(bitcoins$close, order.by = as.Date(bitcoins$Index))
+str(btc)
 
+# d) 
+plot(btc)
 # e)
+
+mean(btc['2017-06'])
+mean(btc['2017-10'])
 
 # f)
 
+range(btc['/2017-08-31'])
+range(btc['2017-09-01/'])
+
 # g) 
+
+weeklyBtcMean <- apply.weekly(btc, mean)
+plot(weeklyBtcMean)
 
 # h)
 
+weeklyBtcSd <- apply.weekly(btc, sd)
+plot(weeklyBtcSd)
+
 # i)
+
+btc_l1 <- lag(btc)
+
+## coredata extracts the values
+## of the time-series (and drops the time index)
+plot(x=coredata(btc_l1), y=coredata(btc), main="Scatterplot BTC against BTC t - 1")
